a33d8341c7d7c50b4972d9bca32cf8d7678c6844
nicolas <nico@MacBook-Pro-de-nicolas.local>
2024-08-07 19:12:38 -0300

    ciudad service and controller created

--- src/ciudad/ciudad.controller.ts
+++ src/ciudad/ciudad.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Ciudad } from './entities/ciudad.entity';
+import { CiudadService } from './ciudad.service';
 
 @Controller('ciudad')
-export class CiudadController {}
+export class CiudadController {
+  constructor(private readonly ciudadService: CiudadService) {}
+
+  @Get('/getAll')
+  async getAll(): Promise<Ciudad[]> {
+    console.log('hello');
+
+    return this.ciudadService.getAll();
+  }
+  @Get('/getById')
+  async getById(): Promise<Ciudad> {
+    console.log('hello');
+
+    return this.ciudadService.getById(1);
+  }
+}
--- src/ciudad/ciudad.service.ts
+++ src/ciudad/ciudad.service.ts
@@ -1,6 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Ciudad } from './entities/ciudad.entity';
 
 @Injectable()
 export class CiudadService {
-  constructor() {}
+  private ciudades: Ciudad[] = [];
+
+  constructor(
+    @InjectRepository(Ciudad)
+    private readonly ciudadRepository: Repository<Ciudad>,
+  ) {}
+
+  public async getAll(): Promise<Ciudad[]> {
+    const ciudades: Ciudad[] = await this.ciudadRepository.find();
+    return ciudades;
+  }
+
+  public async getById(id: number): Promise<Ciudad> {
+    // const criterio: FindOneOptions = { where: { idCiudad: id } };
+    // const ciudad: Ciudad = await this.ciudadRepository.findOne(criterio);
+    const ciudad: Ciudad = await this.ciudadRepository.findOneById(id);
+    if (ciudad) return ciudad;
+  }
 }
