4de141ce90c492df232156b92755471c3351506c
Lucas Fava <lucasfava86@gmail.com>
2024-08-15 18:38:16 -0300

    15-08-2024 feature/Team3

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CiudadModule } from './ciudad/ciudad.module';
+import { EstudianteModule } from './estudiante/estudiante.module';
+import { ProfesorModule } from './profesor/profesor.module';
 
 @Module({
   imports: [
@@ -11,13 +13,15 @@ import { CiudadModule } from './ciudad/ciudad.module';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'root',
+      password: 'root123',
       database: 'escolar',
       synchronize: false,
       entities: ['dist/**/*.entity.js'],
       logging: 'all',
     }),
     CiudadModule,
+    EstudianteModule,
+    ProfesorModule,
   ],
   controllers: [AppController],
   providers: [AppService],
